5432987c778bb2da55d5cbb5c46688ac50ea09c6
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-03-30 17:13:37 +0300

    test v-slot

--- src/components/TestVSlotComponent.vue
+++ src/components/TestVSlotComponent.vue
@@ -1,14 +1,40 @@
 <template>
     <div>
-        
+
     </div>
 </template>
 <script>
 export default {
     name: 'Item',
-    data(){
+    data() {
         return {
-            
+            list: [
+                {
+                    id: 1,
+                    name: 'Item 1',
+                    price: 100,
+                    image: 'https://via.placeholder.com/150x150'
+                },
+                {
+                    id: 2,
+                    name: 'Item 2',
+                    price: 100,
+                    image: 'https://via.placeholder.com/150x150'
+                },
+                {
+                    id: 3,
+                    name: 'Item 3',
+                    price: 100,
+                    image: 'https://via.placeholder.com/150x150'
+                },
+                {
+                    id: 4,
+                    name: 'Item 4',
+                    price: 100,
+                    image: 'https://via.placeholder.com/150x150'
+                },
+                
+            ]
         }
     }
 }
