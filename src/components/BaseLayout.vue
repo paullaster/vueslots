c74e992cf8cdf3a3e14ce8346c39823b87f477a7
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-03-30 18:15:11 +0300

    test v-slot

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -1,5 +1,15 @@
 <template>
-   
+   <div>
+    <header>
+        <slot name="header"></slot>
+    </header>
+    <>
+        <slot name="main"></slot>
+    </main>
+    <footer>
+        <slot name="footer"></slot>
+    </footer>
+   </div>
 </template>
 <script>
 export default {
