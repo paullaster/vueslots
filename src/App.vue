7ee6057d02a505dad55f684cf0294869c7dc83ad
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-03-30 17:09:12 +0300

    test v-slot

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <HelloWorld>
+      <h1>Paullaster Okoth</h1>
+    </HelloWorld>
+  </div>
+</template>
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
