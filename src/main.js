4bfe983264802db45d2156bd67e82bf8961fecc4
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-03-31 12:36:38 +0300

    add files

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router';
 import './style.css'
-import routes from './routes/routes';
+import App from './App.vue'
 
-const router = createRouter({
-    history: createWebHashHistory(),
-    router: routes,
-})
-const app = createApp();
-app.use(router);
-
-app.mount('#app')
+createApp(App).mount('#app')
