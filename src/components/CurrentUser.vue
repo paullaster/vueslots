d74c26f091659386ca2a0449928770fe57408a23
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-03-31 13:45:28 +0300

    add files

---
+++ src/components/CurrentUser.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'CurrentUser',
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
